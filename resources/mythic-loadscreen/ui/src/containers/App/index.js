60fd7856a977492dfecbd13c0a808b58bcca0b3b
QBTreyyy <99930941+QBTreyyy@users.noreply.github.com>
2024-06-09 21:47:15 -0700

    Fix | Mythic Loadscreen | package.json, fontawesome

--- resources/mythic-loadscreen/ui/src/containers/App/index.js
+++ resources/mythic-loadscreen/ui/src/containers/App/index.js
@@ -8,7 +8,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Loadscreen from '../Loadscreen';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fab, fas);
