6c678533de31667fb5c848c0a307c9a05129a2e9
M4RFY <officialsynellgaming@gmail.com>
2024-06-10 18:31:34 +0600

    Removed | Mythic Chat

--- resources/[mythic]/mythic-chat/ui/src/util/Nui.js
+++ resources/[mythic]/mythic-chat/ui/src/util/Nui.js
@@ -5,7 +5,7 @@ export default {
         /// #endif
 
         /* eslint-disable no-unreachable */
-        return fetch(`https://mythic-chat3/${event}`, {
+        return fetch(`https://mythic-chat/${event}`, {
             method: 'post',
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
