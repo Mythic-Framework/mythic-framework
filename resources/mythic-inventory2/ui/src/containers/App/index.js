2c1e455c5405f16d0c805931cf921143724fde0d
QBTreyyy <99930941+QBTreyyy@users.noreply.github.com>
2024-06-09 21:17:23 -0700

    Fix | Mythic Inventory | package.json, fontawesome

--- resources/mythic-inventory2/ui/src/containers/App/index.js
+++ resources/mythic-inventory2/ui/src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
 	CssBaseline,
