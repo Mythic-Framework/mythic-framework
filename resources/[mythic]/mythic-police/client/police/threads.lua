775675bc53b66aaa18da4a61bb6e73b957d7b043
P Gergő <paldi95@gmail.com>
2024-06-16 20:06:56 +0200

    ⚡️ chore: Optimize police thread performance and add weapon switch event handler

--- resources/[mythic]/mythic-police/client/police/threads.lua
+++ resources/[mythic]/mythic-police/client/police/threads.lua
@@ -1,5 +1,11 @@
+local currentWeapon = nil
+
+AddEventHandler('Weapons:Client:SwitchedWeapon', function(weapon, weaponData, weaponItemData)
+	currentWeapon = weapon and joaat(weapon) or nil
+end)
+
 AddEventHandler("Characters:Client:Spawn", function()
-	Citizen.Wait(5000)
+	Wait(5000)
 	StartThreads()
 end)
 
@@ -8,6 +14,7 @@ local _ignored = {
 	[`WEAPON_FIREEXTINGUISHER`] = true,
 	[`WEAPON_FLARE`] = true,
 	[`WEAPON_TASER`] = true,
+	[`WEAPON_STUNGUN`] = true,
 	[`WEAPON_BALL`] = true,
 	[`WEAPON_SNOWBALL`] = true,
 	[`WEAPON_GRENADE`] = true,
@@ -17,94 +24,79 @@ local _ignored = {
 	[`WEAPON_PROXMINE`] = true,
 	[`WEAPON_PIPEBOMB`] = true,
 	[`WEAPON_SMOKEGRENADE`] = true,
-	[`WEAPON_FLARE`] = true,
-	[`WEAPON_FIREEXTINGUISHER`] = true,
-	[`WEAPON_PETROLCAN`] = true,
-}
-
-local _excludes = {
-	{ coords = vector3(1713.17, 2586.68, 59.88), dist = 250 }, -- prison
-	{ coords = vector3(-106.63, 6467.72, 31.62), dist = 45 }, -- paleto bank
-	{ coords = vector3(251.21, 217.45, 106.28), dist = 20 }, -- city bank
-	{ coords = vector3(-622.25, -230.93, 38.05), dist = 10 }, -- jewlery store
-	{ coords = vector3(233.37, 373.31, 106.14), dist = 20 }, -- xgems
-	{ coords = vector3(699.91, 132.29, 80.74), dist = 55 }, -- power 1
-	{ coords = vector3(2739.55, 1532.99, 57.56), dist = 235 }, -- power 2
-	{ coords = vector3(12.53, -1097.99, 29.8), dist = 10 }, -- Adam's Apple / Pillbox Weapon shop
+	[`WEAPON_SNIPERRIFLE2`] = true, -- Hunting Rifle
 }
 
 function StartThreads()
-	Citizen.CreateThread(function()
-		while LocalPlayer.state.loggedIn do
-			local cw = GetSelectedPedWeapon(LocalPlayer.state.ped)
-			local isArmed = false
-			local isOrigin = false
-			if not _ignored[cw] then
-				if not isArmed then
-					if IsPedArmed(LocalPlayer.state.ped, 7) and not IsPedArmed(LocalPlayer.state.ped, 1) then
-						cw = GetSelectedPedWeapon(LocalPlayer.state.ped)
-						isArmed = true
-					end
-				end
-
-				if IsPedShooting(LocalPlayer.state.ped) and not _ignored[cw] then
-					if LocalPlayer.state.onDuty ~= "police" then
-						local veh = GetVehiclePedIsIn(LocalPlayer.state.ped)
-						if veh ~= 0 then
-							EmergencyAlerts:CreateIfReported(500.0, "shotsfiredvehicle", true, {
-								icon = "car",
-								details = string.format("%s", GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(veh))))
-							})
-						elseif IsPedCurrentWeaponSilenced(LocalPlayer.state.ped) then
-							EmergencyAlerts:CreateIfReported(10.0, "shotsfired", true)
-						else
-							EmergencyAlerts:CreateIfReported(900.0, "shotsfired", true)
-						end
-					end
-
-					if
-						IsPedArmed(LocalPlayer.state.ped, 6)
-						and not IsPedSwimming(LocalPlayer.state.ped)
-					then
-						LocalPlayer.state:set("GSR", GlobalState["OS:Time"], true)
-					end
-					Citizen.Wait(60000)
-				end
-			else
-				Citizen.Wait(1000)
-			end
-
-			Citizen.Wait(50)
-		end
-	end)
-
-	Citizen.CreateThread(function()
+	CreateThread(function()
 		while LocalPlayer.state.loggedIn do
 			if LocalPlayer.state.GSR and IsPedSwimming(LocalPlayer.state.ped) then
 				LocalPlayer.state:set("GSR", nil, true)
 			end
-			Citizen.Wait(3000)
-		end
-	end)
-
-	local _blacklistedWeps = {
-		[`WEAPON_UNARMED`] = true,
-		[`WEAPON_FLASHLIGHT`] = true,
-	}
-	Citizen.CreateThread(function()
-		while LocalPlayer.state.loggedIn do
-			if not LocalPlayer.state.isDead then
-				if
-					not _blacklistedWeps[GetCurrentPedWeapon(LocalPlayer.state.ped, true)]
-					and IsPlayerFreeAiming(LocalPlayer.state.PlayerID)
-				then
-					Status.Modify:Add("PLAYER_STRESS", 1, false, true)
-					Citizen.Wait(40000)
-				end
-				Citizen.Wait(100)
-			else
-				Citizen.Wait(10000)
-			end
+			Wait(3000)
 		end
 	end)
 end
+
+local timeOut, stressTimeout, alertTimeout = false, false, false
+AddEventHandler('CEventGunShot', function(entities, eventEntity, args)
+	if timeOut then return end
+	if not currentWeapon then return end
+	if _ignored[currentWeapon] then return end
+	if eventEntity ~= LocalPlayer.state.ped then return end
+
+	timeOut = true
+	SetTimeout(1000, function()
+		timeOut = false
+	end)
+
+	if not stressTimeout then
+		stressTimeout = true
+			Status.Modify:Add("PLAYER_STRESS", 1, false, true)
+		SetTimeout(40000, function()
+			stressTimeout = false
+		end)
+	end
+
+	local luck = math.random(0, 1)
+	if not alertTimeout and LocalPlayer.state.onDuty ~= "police" and luck == 0 then
+		alertTimeout = true
+
+		local veh = GetVehiclePedIsIn(LocalPlayer.state.ped, false)
+
+		if veh ~= 0 then
+			local r, g, b = GetVehicleCustomPrimaryColour(veh)
+
+			local vehModel = GetEntityModel(veh)
+			local vehName = GetLabelText(GetDisplayNameFromVehicleModel(vehModel))
+			if vehName == "NULL" then
+				vehName = GetDisplayNameFromVehicleModel(vehModel)
+			end
+
+			local vehEnt = Entity(veh)
+			if vehEnt and vehEnt.state and vehEnt.state.Make and vehEnt.state.Model then
+				vehName = vehEnt.state.Make .. " " .. vehEnt.state.Model
+			end
+
+			EmergencyAlerts:CreateIfReported(500.0, "shotsfiredvehicle", true, {
+				icon = "car",
+				details = vehName,
+				vehicleColor = {
+					r = r,
+					g = g,
+					b = b,
+				},
+			})
+		elseif IsPedCurrentWeaponSilenced(LocalPlayer.state.ped) then
+			EmergencyAlerts:CreateIfReported(10.0, "shotsfired", true)
+		else
+			EmergencyAlerts:CreateIfReported(900.0, "shotsfired", true)
+		end
+
+		SetTimeout(60000, function()
+			alertTimeout = false
+		end)
+	end
+
+	LocalPlayer.state:set("GSR", GetCloudTimeAsInt(), true)
+end)
